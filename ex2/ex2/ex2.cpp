f3d82b0f86fe744cb9b732bd85fb0ac96714c8eb
Omegalka <134694780+Omegalka@users.noreply.github.com>
2023-10-09 20:52:09 +0300

    Add files via upload

---
+++ ex2/ex2/ex2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+#include "mobile_phone.h"
+#include "human.h"
+#include "weights.h"
+#include "door.h"
+#include "room.h"
+
+int main()
+{
+    Human a;
+    Human b(192, 2);
+    
+}
+
+
+
+
